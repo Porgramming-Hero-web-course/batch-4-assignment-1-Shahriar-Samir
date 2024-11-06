f3575a5190dabcee679d17d958ccf4aa5bb68273
Shahriar-Samir <shabusiness035@gmail.com>
2024-11-06 19:21:29 +0600

    add: problem-1 file

--- problem-1.ts
+++
@@ -1,8 +0,0 @@
-{
-    // the sum of array function 
-    const sumArray = (arr:number[]):number=>{
-        // returning sum of array that contains numbers
-        return arr.reduce((acc,c)=> acc+c)
-    }
-    
-}
